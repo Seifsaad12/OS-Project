e786a844e11f80347e6b4c0cb57293353c285f0a
ammar <ammar@192.168.1.7>
2026-05-02 01:55:16 +0300

    GanttEntry

--- src/os/project/git/OSProjectGit.java
+++ src/os/project/git/OSProjectGit.java
@@ -4,17 +4,39 @@
  */
 package os.project.git;
 
-/**
- *
- * @author TUF
- */
+
+import Model.Process;
+import Model.Process_Queue;
+import Model.GanttEntry;
+import Scheduler.SRTFScheduler;
+import java.util.List;
 public class OSProjectGit {
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class Main {
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        
+        Process_Queue queue = new Process_Queue();
+        queue.addProcess(new Process(1, 0, 8));  // P1: arrival=0, burst=8
+        queue.addProcess(new Process(2, 1, 4));  // P2: arrival=1, burst=4
+        queue.addProcess(new Process(3, 2, 9));  // P3: arrival=2, burst=9
+        queue.addProcess(new Process(4, 3, 5));  // P4: arrival=3, burst=5
+
+        
+        SRTFScheduler scheduler = new SRTFScheduler(queue);
+        scheduler.run();
+
+        // طباعة النتائج
+        scheduler.printResults();
+
+        // التحقق من الـ Gantt Data
+        System.out.println("\n--- Verifying Gantt Data ---");
+        List<GanttEntry> gantt = scheduler.getGanttData();
+        for (GanttEntry entry : gantt) {
+            System.out.println(entry);
+        }
     }
+}
     
 }
