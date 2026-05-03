114023a86380f3b9fd0ddf3081c80a62fe5479ae
Seifsaad12 <seifsaad252006@gmail.com>
2026-05-03 22:53:44 +0300

    RR Success

---
+++ src/Scheduler/RoundRobin.java
@@ -0,0 +1,56 @@
+package Scheduler;
+
+import Model.Process;
+import java.util.*;
+
+public class RoundRobin {
+
+    public static void run(List<Process> processes, int quantum) {
+        Queue<Process> queue = new LinkedList<>();
+        int currentTime = 0;
+        List<Integer> timeline = new ArrayList<>();
+        processes.sort(Comparator.comparingInt(Process::getArrivalTime));
+        int i = 0;
+        queue.add(processes.get(0));
+        i++;
+        while (!queue.isEmpty()) {
+            Process current = queue.poll();
+            if (!current.isStarted()) {
+                current.setStarted(true);
+                current.setResponseTime(currentTime - current.getArrivalTime());
+            }
+            if (current.getRemainingTime() > quantum) {
+                currentTime += quantum;
+                current.setRemainingTime(current.getRemainingTime() - quantum);
+                timeline.add(current.getId());
+
+                while (i < processes.size() && processes.get(i).getArrivalTime() <= currentTime) {
+                    queue.add(processes.get(i));
+                    i++;
+                }
+                queue.add(current);
+            } 
+            else {   
+                currentTime += current.getRemainingTime();
+                current.setRemainingTime(0);
+                current.setFinishTime(currentTime);
+                timeline.add(current.getId());
+                while (i < processes.size() && processes.get(i).getArrivalTime() <= currentTime) {
+                    queue.add(processes.get(i));
+                    i++;
+                }
+            }
+            if (queue.isEmpty() && i < processes.size()) {
+                currentTime = processes.get(i).getArrivalTime();
+                queue.add(processes.get(i));
+                i++;
+            }
+        }
+        System.out.println("Timeline: " + timeline);
+        for (Process p : processes) {
+            System.out.println("P" + p.getId() +
+                    " finished at " + p.getFinishTime() +
+                    " | Response Time: " + p.getResponseTime());
+        }
+    }
+}
